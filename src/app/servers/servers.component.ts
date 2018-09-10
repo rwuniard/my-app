0ab75d88e92895a36481eac64814b3dc12ae9cfa
Ronson Wuniardy <rwuniard@hotmail.com>
2018-09-10 13:54:55 -0400

    Completed first assignment by adding two components. Success-alert and Warning-alert.

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,7 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // This selector for html tag
   selector: 'app-servers',
+
+  // This selector for <div app-servers=""></div>
+  // selector: '[app-servers]',
+
+  // This selector is for <div class="app-server"></div>
+  // selector: '.app-servers',
   template: `
     <app-server></app-server>
     <app-server></app-server>`,
