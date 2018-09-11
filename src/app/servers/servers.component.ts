5723f3dbc51e0a6c7fdb4ebe6c19241271b2c7fb
Ronson Wuniardy <rwuniard@hotmail.com>
2018-09-11 17:39:37 -0400

    Completed first assignment by adding two components. Success-alert and Warning-alert.

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,16 +9,37 @@ import { Component, OnInit } from '@angular/core';
 
   // This selector is for <div class="app-server"></div>
   // selector: '.app-servers',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>`,
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>
+  // ',
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = '';
+  serverCreated = false;
+  servers = ['TestServer', 'TestServer2'];
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit() {
   }
 
+  onClickAddServer() {
+    this.serverCreationStatus = 'Server was created. Name is ' + this.serverName;
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+  }
+
+  onUpdateServerName(event: any) {
+    console.log(event);
+    this.serverName = event.target.value;
+  }
 }
